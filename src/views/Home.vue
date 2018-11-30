7dd8c522c44166ba194a840ea3bf6b8cf5be825a
hjm <2445543801@qq.com>
2018-11-30 11:16:07 +0800

    实现首页初始化

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,116 @@
 <template>
   <div class="home">
-    首页
+    <el-container>
+      <el-aside width="200px">
+        <div class="logo"></div>
+        <el-menu
+          default-active="2"
+          class="el-menu-vertical-demo"
+          @open="handleOpen"
+          @close="handleClose"
+          background-color="#545c64"
+          text-color="#fff"
+          active-text-color="#ffd04b"
+          :unique-opened='true'
+          :router='true'
+        >
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>用户管理</span>
+            </template>
+            <el-menu-item index="1-1">
+              <template slot="title">
+                <i class="el-icon-menu"></i>
+                <span>用户列表</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+          <el-submenu index="2">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>用户管理</span>
+            </template>
+            <el-menu-item index="#">
+              <template slot="title">
+                <i class="el-icon-menu"></i>
+                <span>用户列表</span>
+              </template>
+            </el-menu-item>
+            <el-menu-item index="#">
+              <template slot="title">
+                <i class="el-icon-menu"></i>
+                <span>用户列表</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+
+        </el-menu>
+      </el-aside>
+      <el-container>
+        <el-header>Header</el-header>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 <script>
-import { GetUserList } from '@/api'
 export default {
   data () {
     return {}
   },
-  mounted () {
-    GetUserList({ pagenum: 1, pagesize: 10 }).then(res => {
-      console.log(res)
-    })
+  methods: {
+    handleOpen (key, keyPath) {
+      console.log(key, keyPath)
+    },
+    handleClose (key, keyPath) {
+      console.log(key, keyPath)
+    }
   }
 }
 </script>
 <style lang="scss" scoped>
+.home {
+  height: 100%;
+  .el-menu-admin:not(.el-menu--collapse) {
+    width: 200px;
+    min-height: 400px;
+  }
+  .el-container {
+    height: 100%;
+  }
+  .el-aside {
+    background-color: #545c64;
+  }
+  .el-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: #545c64;
+  }
+  .logo {
+    height: 60px;
+    background: url(../assets/logo.png);
+    background-size: cover;
+    background-color: #fff;
+  }
+  .toggle-btn {
+    padding: 0 15px;
+    margin-left: -20px;
+    font-size: 36px;
+    color: white;
+    cursor: pointer;
+    line-height: 60px;
+    &:hover {
+      background-color: #4292cf;
+    }
+  }
+  .system-title {
+    font-size: 28px;
+    color: white;
+  }
+  .welcome {
+    color: white;
+  }
+}
 </style>
